6c62bf2a895fa6e3227293c34542771758f3012e
nilayjain <nilayjain13@gmail.com>
2016-09-23 20:26:42 +0530

    added 3pc

--- q2/3pc.py
+++ q2/3pc.py
@@ -1,15 +1,38 @@
 from mpi4py import MPI
 import os
 import sys
-
+import random
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
 bank = {}
 message = []
+leader = 0
+alive = range(0, size)
 
-# userid, debit/depo/view, amount, time, node_num, failure stage
+def get_status():
+	# all RM's tell TM agree/disagree to prepare.
+	count = 1
+	if message[1] != 'v' and message[2] == 0:
+		count = 0
+	if rank != leader:
+		comm.send(count, dest = leader)
+
+	if rank == leader:
+		for j in range(1, len(alive)):
+			x = comm.recv(source = j)
+			
+			if x == 1:
+				count = count + 1
+			else:
+				count = 0
+	
+	# send no. of agreeing processes
+	# so that everyone aborts if they want to
+	count = comm.bcast(count,root = leader)
+	return count
+
 if __name__ == "__main__":
 	with open(sys.argv[1]) as f:
 		content = f.readlines()
@@ -31,6 +54,7 @@ if __name__ == "__main__":
 			if lis[1] == 'f':
 				print "failure in node " + str(rank) + " , aborting..."
 				comm.Abort()
+
 			if i + 1 < past + 2 + num_queries:
 				lis1 = content[i + 1].split()
 				# if debitting and userid and timestamp are same.
@@ -47,7 +71,6 @@ if __name__ == "__main__":
 					if data > bank[lis[0]]:
 						print 'aborting transaction, insufficient balance'
 						data = 0
-
 			else:
 				data = int(lis[2])
 				if data > bank[lis[0]]:
@@ -58,49 +81,70 @@ if __name__ == "__main__":
 			message.append(lis[1])
 			message.append(data)
 			message.append(flag)
-			comm.send(message, dest = 0)
-		
-		# prepare TM		
-		if rank == 0:
+			message.append(lis[5])
+			comm.send(message, dest = leader)
+
+
+		# stage 1
+		# TM prepared.	
+		if rank == leader:
 			message = comm.recv(source=MPI.ANY_SOURCE)
 			if message[1] == 'v':
 				print "balance of user " + message[0] + " " + str(bank[message[0]])
 		
-		# TM prepares other RMs.
-		message = comm.bcast(message, root = 0)
- 
-		# all RM's tell TM agree/disagree.
-		count = 1
-		if message[1] != 'v' and message[2] == 0:
-			count = 0
-		if rank != 0:
-			comm.send(count, dest = 0)
+		# TM prepares all RM's.
+		message = comm.bcast(message, root = leader)
 
-		if rank == 0:
-			for j in range(1,size ):
-				x = comm.recv(source = j)
-				
-				if x == 1:
-					count = count + 1
-				else:
-					count = 0
+		
+		count = get_status()
+		
+		# stage 1 end
 
-		# TM shares no. of agree-ing RM's with all RM's
-		a = comm.bcast(count, root = 0)
-		if a == size:
+		# TM fails...
+		if message[4] == 2:
+			alive.remove(leader)
+			if rank == leader:
+				sys.exit(0)
+			
+			# elect new TM.
+			leader = random.choice(alive)		
+			leader = comm.bcast(leader, root = leader)
+			count = get_status()
+
+		if count != len(alive) or message[4] == 1:
+			print "Aborting at node ",rank
+			if message[3] == 0: i = i + 1
+			else: i = i + 2
+			continue
+		
+		## pre commit phase
+		# send data to all nodes (prepare to commit)
+		message = comm.bcast(message, root = leader)
+		
+		# recieved ack from RM's
+		count = get_status()		
+		if count != len(alive) or message[4] == 1:
+			print "Aborting at node ",rank
+		# commit..
+		else:
 			print "commiting at node :" ,rank 
 			if message[1] == 'd':
 				bank[message[0]] -= int(message[2])	
 			if message[1] == 'c':
-				bank[message[0]] += int(message[2])			
-		else:
-			print "aborting at node :" ,rank
+				bank[message[0]] += int(message[2])
 
 		if message[3] == 0: i = i + 1
 		else: i = i + 2
-
 
 	print "program khatam " + str(rank)
 
 	print bank,
 	print " " + str(rank)
+
+
+		
+
+
+
+			
+
