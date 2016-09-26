59726ac4303af1a890368e7d7412300ca3c876f8
nilayjain <nilayjain13@gmail.com>
2016-09-26 21:54:50 +0530

    mpi q1 works with mongo

--- mpi/inverted_index.py
+++ mpi/inverted_index.py
@@ -5,6 +5,7 @@ import numpy as np
 import nltk
 import math
 import pickle
+from pymongo import MongoClient
 data = []
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -31,28 +32,25 @@ def tokenize_list(doc_text):
     tokens = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(doc_text)
 	# filtered_words = [word for word in tokens if word not in nltk.corpus.stopwords.words('english')]
     tokens = [i.lower() for i in tokens]
+    tokens = [i.decode('utf-8', 'ignore').encode("utf-8") for i in tokens]
     porter = nltk.stem.PorterStemmer()
     return [porter.stem(i) for i in tokens]
 
-# save/load pickle modules...
-'''
-def save_obj(obj, name ):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def load_obj(name ):
-    with open('obj/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
-'''
+def store_db():
+	client = MongoClient()
+	db = client['inv_index' + str(rank)]
+	word_col = db.word_col
+	for key, value in index.iteritems():
+		post = {'word': key, 'word_dict': value}
+		word_col.insert_one(post)
 
+# for normal dataset..
 # process files and store data in inverted index.
 def process_files(src, files):
 	for file in files:
 		doc_name = src + '/' + file
 		words = tokenize_list(open(doc_name).read())
-		# if file == 'doc0000':
-		# 	print words
-		# gives the index of the current word.
 		fptr = 0
 		for word in words:
 			word_dict = index[word] if word in index else {}
@@ -62,8 +60,47 @@ def process_files(src, files):
 			index[word] = word_dict
 			fptr += len(word)
 
-	with open("index" + str(rank), "wb") as filename:
-		pickle.dump(index, filename)
+	store_db()
+
+# with preprocessing.
+# def process_files(src, files):
+# 	for file in files:
+# 		filename = src + '/' + file
+# 		words = tokenize_list(open(filename).read())
+# 		# gives the index of the current word.
+# 		start = 0
+# 		end = 0
+# 		doc_name = ""
+# 		fptr = 0
+# 		for word in words:
+# 			if start < 2:
+# 				start += 1
+# 				continue
+# 			if start == 2:
+# 				doc_name = word
+# 				start += 1
+# 				fptr = 0
+# 				continue
+# 			if start > 2 and start <= 10:
+# 				start += 1
+# 				continue
+
+# 			if word == 'endofarticl':
+# 				end = end + 1
+# 				continue
+# 			if end != 0:
+# 				end = 0
+# 				start = 0
+# 				continue
+
+# 			word_dict = index[word] if word in index else {}
+# 			location_list = word_dict[doc_name] if doc_name in word_dict else []
+# 			location_list.append(fptr)
+# 			word_dict[doc_name] = location_list
+# 			index[word] = word_dict
+# 			fptr += len(word)
+# 	store_db()
+
 
 
 if __name__ == "__main__":
