57c597ccaa5d4f3257e23e067ed08b1acec91b32
nilayjain <nilayjain13@gmail.com>
2016-09-27 20:20:10 +0000

    inverted index exception handling

--- mpi/inverted_index.py
+++ mpi/inverted_index.py
@@ -1,7 +1,6 @@
 from mpi4py import MPI
 import os
 import sys
-import numpy as np
 import nltk
 import math
 import pickle
@@ -29,25 +28,32 @@ def send_filenames(src):
 
 # this function returns a list of tokenized and stemmed words of any text
 def tokenize_list(doc_text):
-    tokens = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(doc_text)
-	# filtered_words = [word for word in tokens if word not in nltk.corpus.stopwords.words('english')]
-    tokens = [i.lower() for i in tokens]
-    tokens = [i.decode('utf-8', 'ignore').encode("utf-8") for i in tokens]
-    porter = nltk.stem.PorterStemmer()
-    return [porter.stem(i) for i in tokens]
+	tokens = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(doc_text)
+	tokens = [word for word in tokens if word not in nltk.corpus.stopwords.words('english')]
+	tokens = [i.lower() for i in tokens]
+	tokens = [i.decode('utf-8', 'ignore').encode("utf-8") for i in tokens]
+	porter = nltk.stem.PorterStemmer()
+	return [porter.stem(i) for i in tokens]
 
 
 def store_db():
+	print 'storing in database at rank = ' + str(rank)
 	client = MongoClient()
 	db = client['inv_index' + str(rank)]
 	word_col = db.word_col
 	for key, value in index.iteritems():
 		post = {'word': key, 'word_dict': value}
-		word_col.insert_one(post)
+		try:
+			word_col.insert_one(post)
+		except:
+			pass
 
+	print 'processing completed at rank = ' + str(rank)
 # for normal dataset..
 # process files and store data in inverted index.
 def process_files(src, files):
+	count = len(files)
+	done = 0
 	for file in files:
 		doc_name = src + '/' + file
 		words = tokenize_list(open(doc_name).read())
@@ -59,49 +65,10 @@ def process_files(src, files):
 			word_dict[file] = location_list
 			index[word] = word_dict
 			fptr += len(word)
+		done = done + 1
+		print file + ' processed,' + str(done) + ' / ' + str(count) + ' done at rank = ' + str(rank)
 
 	store_db()
-
-# with preprocessing.
-# def process_files(src, files):
-# 	for file in files:
-# 		filename = src + '/' + file
-# 		words = tokenize_list(open(filename).read())
-# 		# gives the index of the current word.
-# 		start = 0
-# 		end = 0
-# 		doc_name = ""
-# 		fptr = 0
-# 		for word in words:
-# 			if start < 2:
-# 				start += 1
-# 				continue
-# 			if start == 2:
-# 				doc_name = word
-# 				start += 1
-# 				fptr = 0
-# 				continue
-# 			if start > 2 and start <= 10:
-# 				start += 1
-# 				continue
-
-# 			if word == 'endofarticl':
-# 				end = end + 1
-# 				continue
-# 			if end != 0:
-# 				end = 0
-# 				start = 0
-# 				continue
-
-# 			word_dict = index[word] if word in index else {}
-# 			location_list = word_dict[doc_name] if doc_name in word_dict else []
-# 			location_list.append(fptr)
-# 			word_dict[doc_name] = location_list
-# 			index[word] = word_dict
-# 			fptr += len(word)
-# 	store_db()
-
-
 
 if __name__ == "__main__":
 	files = []
@@ -112,6 +79,6 @@ if __name__ == "__main__":
 		files = send_filenames(sys.argv[1])
 	else:
 		files = comm.recv(source = 0)
-
+	print 'filenames sent.'
 	process_files(sys.argv[1], files)
 
